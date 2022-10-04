8c17af98e0315036805e2063f33c1f017e660bce
Bertinatti <leonardocardoso13@live.com>
2022-10-04 20:08:32 -0300

    Finalizando as alterações do exercício.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string modelo;
+        private string imei;
+        private string memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, string memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.modelo = modelo;
+            this.imei = imei;
+            this.memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Testando a implementação para a classe Nokia
+Nokia nokia = new Nokia(numero: "(11) 99999-9999", modelo: "Nokia Xperia XA1 PLUS", imei: "99999999", memoria: "128 GB");
+nokia.Ligar();
+nokia.InstalarAplicativo("WhastApp");
+nokia.ReceberLigacao();
+
+Console.WriteLine();
+
+// Testando a implementação para a classe Iphone
+Iphone iphone = new Iphone(numero: "(11) 99999-9999", modelo: "Iphone 14 PLUS", imei: "66666666", memoria: "512 GB" );
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook App.");
+iphone.ReceberLigacao();
